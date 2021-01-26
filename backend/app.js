620c6b107630a89a3011b0a39b7b36222296093b
jaewan <stpjw777@gmail.com>
2021-01-26 17:01:28 +0900

    mongodb connect

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,19 @@
 const express = require("express");
-const mongoose = require("mongo`");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:p@ssw0rd@cluster0.axtok.mongodb.net/todolist?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log("connected mongodb");
+  })
+  .catch(error => {
+    console.error(error);
+  });
 
 const port = 3010;
 
